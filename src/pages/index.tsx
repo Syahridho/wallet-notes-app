d46fd47f16e1120bb03f529655eb7904e2998ded
syahridho arjuna syahputra <syahridhosyahputra@gmail.com>
2024-11-12 14:18:42 +0700

    add title each pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { signIn, useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import Dashboard from "@/components/pages/dashboard";
 import { FaCircleNotch } from "react-icons/fa6";
+import Head from "next/head";
 
 export default function Home() {
   const { data: session, status } = useSession() as {
@@ -11,25 +12,43 @@ export default function Home() {
 
   if (status === "loading") {
     return (
-      <div className="min-h-screen flex justify-center items-center">
-        <FaCircleNotch className="animate-spin text-slate-900/30 w-16 h-16" />
-      </div>
+      <>
+        <Head>
+          <title>Home</title>
+        </Head>
+        <div className="min-h-screen flex justify-center items-center">
+          <FaCircleNotch className="animate-spin text-slate-900/30 w-16 h-16" />
+        </div>
+      </>
     );
   }
 
   if (status === "authenticated" && session) {
-    return <Dashboard />;
+    return (
+      <>
+        <Head>
+          <title>Home</title>
+        </Head>
+        <Dashboard />
+      </>
+    );
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <Button onClick={() => signIn()} className="mb-4">
-        Sign In
-      </Button>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
 
-      {session?.user?.fullname && (
-        <h1 className="text-lg font-semibold">{session.user.fullname}</h1>
-      )}
-    </div>
+      <div className="flex flex-col items-center justify-center min-h-screen p-4">
+        <Button onClick={() => signIn()} className="mb-4">
+          Sign In
+        </Button>
+
+        {session?.user?.fullname && (
+          <h1 className="text-lg font-semibold">{session.user.fullname}</h1>
+        )}
+      </div>
+    </>
   );
 }
